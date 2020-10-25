797005897f301aa20a4a2ee3e396d1e33384d4d2
P48640 <70651831+P48640@users.noreply.github.com>
2020-10-25 15:09:17 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,41 @@
+let short = 0
+let medium = 0
+let tall = 0
 let height = 0
 let random = 0
 let cont_even = 0
 let cont_odd = 0
+let counter = 0
 let angle = 0
 let Dice = 0
 let Dice_2 = 0
 let L = 0
+let x = 0
+let y = 0
 input.onPinPressed(TouchPin.P0, function () {
-    for (let index = 0; index < 50; index++) {
+    short = 0
+    medium = 0
+    tall = 0
+    for (let index = 0; index < 10; index++) {
         height = randint(100, 200)
         basic.showNumber(height)
         if (height <= 150) {
             basic.showString("Short Person")
+            short += 1
         } else if (151 <= height && height <= 170) {
             basic.showString("Medium Person")
+            medium += 1
         } else if (height > 171) {
             basic.showString("Tall Person")
+            tall += 1
         }
     }
+    basic.showString("Short Person")
+    basic.showNumber(short)
+    basic.showString("Medium Person")
+    basic.showNumber(medium)
+    basic.showString("Tall Person")
+    basic.showNumber(tall)
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 10; index++) {
@@ -39,6 +57,7 @@ input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
 })
 input.onPinPressed(TouchPin.P1, function () {
+    counter = 0
     for (let index = 0; index < 10; index++) {
         angle = randint(0, 180)
         basic.showNumber(angle)
@@ -48,8 +67,11 @@ input.onPinPressed(TouchPin.P1, function () {
             basic.showString("Straight")
         } else if (angle > 90) {
             basic.showString("Obtuse")
+            counter += 1
         }
     }
+    basic.showString("Total_Obtuse")
+    basic.showNumber(counter)
 })
 input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 1; index++) {
@@ -77,4 +99,18 @@ input.onGesture(Gesture.Shake, function () {
         }
     }
     basic.clearScreen()
+})
+basic.forever(function () {
+    while (input.pinIsPressed(TouchPin.P2)) {
+        x = randint(0, 5)
+        y = randint(0, 5)
+        led.plot(x, y)
+        basic.pause(300)
+        if (x == y) {
+            music.playMelody("G E F A E B C E ", 120)
+        } else {
+            music.playTone(698, music.beat(BeatFraction.Whole))
+        }
+        basic.clearScreen()
+    }
 })
